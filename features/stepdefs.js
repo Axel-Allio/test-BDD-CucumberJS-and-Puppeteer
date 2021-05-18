318ddda556f8c84a0ced5a3b84f1b8552d259a66
AxelAllio <axel.allio@algoan.com>
2021-05-18 18:57:01 +0200

    ajout after

--- features/stepdefs.js
+++ features/stepdefs.js
@@ -1,6 +1,7 @@
 const { Given, When, Then } = require('@cucumber/cucumber');
 const assert = require('assert');
 const puppeteer = require('puppeteer')
+var {After, Before} = require('@cucumber/cucumber');
 
 Given('The browser is open', async function () {
     this.browser = await puppeteer.launch({})
@@ -17,4 +18,17 @@ Then('the title should be {string}', async function (string) {
     })
     var res = title.split(" | ")
     assert.strictEqual(string, res[1])
+    //await browser.close();
 });
+
+// BeforeAll(async function() {
+//     this.browser = await puppeteer.launch({})
+// })
+
+After(async function() {
+    await this.browser.close();
+    //return Promise.resolve()
+    
+  });
+
+  // a voir : garder meme instance de browser pour tous les scénarios
